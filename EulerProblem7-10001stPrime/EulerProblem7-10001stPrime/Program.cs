b5a10eee82ff6fd172c37097a695404a1493f04c
Ovidiu Othoze <ovidiu.othoze@yahoo.com>
2018-07-05 20:38:41 +0300

    Euler problem 7 all in main

---
+++ EulerProblem7-10001stPrime/EulerProblem7-10001stPrime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem7_10001stPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primeNumbersList = new List<int>();
+            primeNumbersList.Add(2);
+            primeNumbersList.Add(3);
+            int currentOrder = primeNumbersList.Count;
+            bool prime = true;
+            int primeTestNumber = 5;
+            int i = 0;
+            while (currentOrder <= 10001)
+            {
+                prime = true;
+                for (i = 0; i < currentOrder; i++)
+                {
+                    if (primeTestNumber % primeNumbersList[i] == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime == true)
+                {
+                    primeNumbersList.Add(primeTestNumber);
+                    currentOrder += 1;
+                }
+                primeTestNumber += 1;
+                i = 0;
+            }
+            Console.WriteLine(primeNumbersList[10001 - 1]);
+
+        }
+
+    }
+}
+
+

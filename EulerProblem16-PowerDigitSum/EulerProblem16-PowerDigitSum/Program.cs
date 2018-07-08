835de986a005cf15ab28a254a85b2bb0099b7655
Ovidiu Othoze <ovidiu.othoze@yahoo.com>
2018-07-08 17:37:08 +0300

    Solved using BigInteger. Added reference to System.Numerics in visual studio assemblies.

---
+++ EulerProblem16-PowerDigitSum/EulerProblem16-PowerDigitSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace EulerProblem16_PowerDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                BigInteger number = (BigInteger)Math.Pow(2, 1000);
+            Console.WriteLine(digitSum(number));
+        }
+
+        public static int digitSum(BigInteger number)
+        {
+            int sum=0;
+            while(number>0)
+            {
+                sum += (int)(number % 10);
+                number = number / 10;
+            }
+            return sum;
+        }
+    }
+}
+

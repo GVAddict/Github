b45bfa4b8fb9a682b73f529a2cb556fd118db0ba
Ovidiu Othoze <ovidiu.othoze@yahoo.com>
2018-07-08 16:29:57 +0300

    Working

---
+++ EulerProblem14-LongestCollatzSequence/EulerProblem14-LongestCollatzSequence/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Longest Collatz sequence
+Problem 14 
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
+
+namespace EulerProblem14_LongestCollatzSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<int,int> sequenceSizeDictionary = new Dictionary<int, int>();
+            for (int k = 1; k <= 1000000; k++)
+            {
+                int termsNumber = sequenceSize(k);
+                sequenceSizeDictionary.Add(k, termsNumber);
+            }
+            /*foreach(var pair in sequenceSizeDictionary) //displays number - sequence size list
+            {
+                Console.WriteLine("{0}:{1}", pair.Key,pair.Value);
+            }*/
+            //Console.WriteLine("{0}", sequenceSizeDictionary.Values.Max());
+            int maxSequence = sequenceSizeDictionary.Values.Max();
+            foreach (var pair in sequenceSizeDictionary)
+            {
+                if(maxSequence==pair.Value)
+                {
+                    Console.WriteLine("Number with max number of terms in sequence is {0} with {1} terms",pair.Key,pair.Value);
+                }
+            }
+        }
+
+        public static int sequenceSize(long n)//method returns number of terms in sequence for a given int
+        {
+            int i = 1;
+            while (n > 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = n * 3 + 1;
+                }
+                i++;
+            }
+            return i;
+        }
+
+    }
+}
